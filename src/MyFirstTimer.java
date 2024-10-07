2c2ab332b45c4beb764ebc115298fc083fc4285e
Nathan36353 <nathan36353@gmail.com>
2024-10-06 21:13:04 -0700

    Delay: Time between each trigger, in milliseconds.
    ActionListener: Code to run on each trigger.
    
    Start: Call start() to activate the timer.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,23 +1,56 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int MAX_STEPS = 20;
-	private GLabel myLabel;
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
+    public static final int PROGRAM_HEIGHT = 600;
+    public static final int PROGRAM_WIDTH = 800;
+    private GLabel myLabel;
+    private Timer timer;
+    private int numTimes; 
 
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
-		add(myLabel);
-	}
-	
-	public static void main(String[] args) {
-		new MyFirstTimer().start();
-	}
+    public void init() {
+        setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+        requestFocus();
+        
+        myLabel = new GLabel("times called? 0", 0, 100);
+        add(myLabel);
+    }
+    
+    public void run() {
+
+        numTimes = 0;
+        
+       
+        timer = new Timer(1000, this);
+        
+  
+        timer.setInitialDelay(3000);
+        
+       
+        timer.start();
+    }
+
+  
+    public void actionPerformed(ActionEvent e) {
+        
+        numTimes++;
+        
+      
+        if (numTimes >= 10) {
+            timer.stop();
+            return;  
+        }
+        
+       
+        myLabel.move(5, 0);
+        
+       
+        myLabel.setLabel("times called? " + numTimes);
+    }
+
+    public static void main(String[] args) {
+        new MyFirstTimer().start();
+    }
 }
