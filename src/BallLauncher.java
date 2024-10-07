aa53fbec4d5c7dde2197bd60c5e3df9c5e116465
Nathan36353 <nathan36353@gmail.com>
2024-10-06 21:20:27 -0700

    Using pause() in an actionPerformed method can freeze the program.

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -5,34 +5,55 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class BallLauncher extends GraphicsProgram{
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int SIZE = 25;
-	
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public void run() {
-		addMouseListeners();
-	}
-	
-	public void mousePressed(MouseEvent e) {
-		GOval ball = makeBall(SIZE/2, e.getY());
-		add(ball);
-	}
-	
-	public GOval makeBall(double x, double y) {
-		GOval temp = new GOval(x-SIZE/2, y-SIZE/2, SIZE, SIZE);
-		temp.setColor(Color.RED);
-		temp.setFilled(true);
-		return temp;
-	}
-	
-	public static void main(String[] args) {
-		new BallLauncher().start();
-	}
+public class BallLauncher extends GraphicsProgram {
+    public static final int PROGRAM_HEIGHT = 600;
+    public static final int PROGRAM_WIDTH = 800;
+    public static final int SIZE = 25;
+    public static final int MS = 50; 
+    public static final int SPEED = 2; 
 
+    private ArrayList<GOval> balls; 
+    private Timer movement; 
+
+    public void init() {
+        setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+        requestFocus();
+        balls = new ArrayList<GOval>(); 
+    }
+
+    public void run() {
+        addMouseListeners();
+        movement = new Timer(MS, this); 
+        movement.start(); 
+    }
+
+    public void mousePressed(MouseEvent e) {
+        
+        for (GOval ball : balls) {
+            if (ball.getX() < 100) {
+                return; 
+            }
+        }
+        GOval ball = makeBall(SIZE / 2, e.getY());
+        add(ball);
+        balls.add(ball); 
+    }
+
+    public GOval makeBall(double x, double y) {
+        GOval temp = new GOval(x - SIZE / 2, y - SIZE / 2, SIZE, SIZE);
+        temp.setColor(Color.RED);
+        temp.setFilled(true);
+        return temp;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        for (GOval ball : balls) {
+            ball.move(SPEED, 0); 
+        }
+    }
+
+    public static void main(String[] args) {
+        new BallLauncher().start();
+    }
 }
+
